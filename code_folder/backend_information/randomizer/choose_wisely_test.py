1c7c8deeb5a7709bd601226cc32a30b85137bd80
Patrick May <patjster@gmail.com>
2023-06-08 10:39:48 -0500

    Made it so it can now upload to a text file instead of terminal

--- code_folder/backend_information/randomizer/choose_wisely_test.py
+++ code_folder/backend_information/randomizer/choose_wisely_test.py
@@ -2,12 +2,15 @@
 import sys
 import os
 import random
+from datetime import datetime
 
 os.chdir("code_folder/")
 sys.path.append(os.getcwd())
 
 
 import backend_information.races.race_table as r_table
+
+current_day = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
 
 def random_pick():
@@ -19,195 +22,243 @@ def random_pick():
             case "Beastman":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Beastman)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Bretonnia":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Bretonnia)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Chaos Dwarfs":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Chaos_Dwarfs)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Daemons of Chaos":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Daemons_of_Chaos)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Dark Elves":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Dark_Elves)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Dwarfs":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Dwarfs)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "The Empire":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.The_Empire)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Grand Cathay":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Grand_Cathay)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Greenskins":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Greenskins)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "High Elves":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.High_Elves)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Khorne":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Khorne)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Kislev":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Kislev)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Lizardmen":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Lizardmen)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Norsca":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Norsca)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Nurgle":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Nurgle)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))   
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result   
             case "Ogre Kingdoms":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Ogre_Kingdoms)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Skaven":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Skaven)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Slaanesh":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Slaanesh)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Tomb Kings":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Tomb_Kings)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Tzeentch":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Tzeentch)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Vampire Coast":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Vampire_Coast)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Vampire Counts":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Vampire_Counts)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Warriors of Chaos":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Warriors_of_Chaos)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Wood Elves":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Wood_Elves)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
     elif previous_race.lower() == "none":
         random_race = random.choice(r_table.Races)
         match random_race:
             case "Beastman":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Beastman)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Bretonnia":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Bretonnia)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Chaos Dwarfs":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Chaos_Dwarfs)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Daemons of Chaos":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Daemons_of_Chaos)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Dark Elves":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Dark_Elves)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Dwarfs":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Dwarfs)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "The Empire":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.The_Empire)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Grand Cathay":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Grand_Cathay)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Greenskins":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Greenskins)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "High Elves":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.High_Elves)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Khorne":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Khorne)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Kislev":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Kislev)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Lizardmen":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Lizardmen)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Norsca":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Norsca)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Nurgle":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Nurgle)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))   
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result   
             case "Ogre Kingdoms":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Ogre_Kingdoms)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Skaven":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Skaven)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Slaanesh":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Slaanesh)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Tomb Kings":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Tomb_Kings)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Tzeentch":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Tzeentch)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Vampire Coast":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Vampire_Coast)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Vampire Counts":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Vampire_Counts)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Warriors of Chaos":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Warriors_of_Chaos)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
             case "Wood Elves":
                 Race = random_race
                 Legendary_Lord = random.choice(r_table.Wood_Elves)
-                print("Race: {}\nLegendary Lord: {}".format(Race, Legendary_Lord))
+                result = ["Race: {}\nLegendary Lord: {}\n{}".format(Race, Legendary_Lord, current_day)]
+                return result
--- code_folder/main_code.py
+++ code_folder/main_code.py
@@ -1,9 +1,18 @@
 # This file is to bring functions up to the front for ease of use
 # This way all other files will need to be updated for updates instead of this main file
+from datetime import datetime
+
 
 # import backend_information.randomizer.choose_wisely as c_wisely
 import backend_information.randomizer.choose_wisely_test as c_wisely_v2
 
-# c_wisely.random_pick()
-c_wisely_v2.random_pick()
+def write_file(data):
+    with open("C:/Users/patjs/Desktop/Warhammer_Race_Pick.txt", "w") as f:
+        f.writelines(data)
+        f.close()
 
+def run():
+    data = c_wisely_v2.random_pick()
+    write_file(data)
+
+run()
